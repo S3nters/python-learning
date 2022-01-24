5b3ac094b606b60065f8c4a7c617de09fc4798e3
S3nters <98290021+S3nters@users.noreply.github.com>
2022-01-24 22:16:21 +0100

    Update event_time.py
    Optimalizing

--- event_time.py
+++ event_time.py
@@ -2,11 +2,9 @@ h = int(input("Czas startu (godziny): "))
 m = int(input("Czas startu (minuty): "))
 d = int(input("Czas trwania wydarzenia (minuty): "))
 
-event_time_h = (d + m) // 60
+event_time_h = (h + ((d + m) // 60)) % 24
+
 event_time_min = (d + m) % 60
 
-end_time_h = (h + event_time_h) % 24
-
 # print(event_time_h, event_time_min)
-
-print("Czas zakończenia wydarzenia: ", end_time_h, ":", event_time_min)
+print("Czas zakończenia wydarzenia: ", event_time_h, ":", event_time_min)
